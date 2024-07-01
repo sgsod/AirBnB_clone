ea62c3466c204fe9b05daab0b8ed28039a8e0183
sgsod <gamanorman3@gmail.com>
2024-07-01 12:56:38 +0200

    console.py docstrings

--- console.py
+++ console.py
@@ -1,25 +1,53 @@
 #!/usr/bin/python3
-"""Console: command interpreter"""
-
-
 import cmd
 
+"""
+Console command interpreter.
+
+Class:
+
+    HNBHCommand
+"""
+
+
 class HBNBCommand(cmd.Cmd):
-    self.prompt = "(hbnb)"
+    """
+    Shell class for HBNB project
+
+    Attributes:
+        prompt : str
+            prompt at the beginning of line
+
+    Methods:
+        emptyline
+        EOF
+        exit
+"""
+
+
+    def __init__(self):
+        self.prompt = "(hbnb)"
     
     def emptyline(self):
-        """parse empty line without repeating last command"""
+        """
+        parse empty line without repeating last command
+        """
+
         pass
 
     def do_exit(self):
-        """exit interpreter"""
+        """
+        exit interpreter
+        """
+
         return True
 
     def do_EOF(self, line):
-        """Exit the interpreter"""
-        return True
+        """
+        Exit the interpreter
+        """
 
-    
+        return True
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
